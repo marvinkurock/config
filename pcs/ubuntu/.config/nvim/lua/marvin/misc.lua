9a46208b91e777d48c16c45c69a566c6c69dbf38
Marvin <marvin.kurock@tech-excellence.de>
2022-12-01 08:25:42 +0100

    link

--- pcs/ubuntu/.config/nvim/lua/marvin/misc.lua
+++
@@ -1,9 +0,0 @@
-require("Comment").setup()
-
-require('nvim-autopairs').setup{}
-
-require("colorizer").setup()
-
-require("diffview").setup {
-  enhanced_diff_hl = true,
-}
--- pcs/ubuntu/.config/nvim/lua/marvin/theme.lua
+++
@@ -1,7 +0,0 @@
-local catppuccin = require("catppuccin")
-catppuccin.setup {
-  flavour = "mocha"
-}
-vim.cmd[[colorscheme catppuccin]]
--- vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_italic_functions = true
