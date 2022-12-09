edef101e07fe6377d79804057a7f1cf39e077507
marvin <mk@kk.com>
2022-12-09 12:12:16 +0100

    pwoertmenu

--- vms/parrotos/.config/scripts/powermenu.sh
+++ vms/parrotos/.config/scripts/powermenu.sh
@@ -2,7 +2,7 @@ choice=$(echo -n 'Shutdown\nRestart\nLogout' | rofi -dmenu)
 echo $choice
 
 case $choice in
-  'Shutdown') poweroff ;;
-  'Restart') restart ;;
+  'Shutdown') systemctl poweroff ;;
+  'Restart') systemctl reboot ;;
   'Logout') i3-msg exit ;;
 esac
