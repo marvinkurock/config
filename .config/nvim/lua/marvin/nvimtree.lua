c38288f1358e8d36d22f1a9a2edd9b4f233e573b
Marvin <marvin.kurock@tech-excellence.de>
2023-01-31 10:14:09 +0100

    nvimtree update

--- .config/nvim/lua/marvin/nvimtree.lua
+++ .config/nvim/lua/marvin/nvimtree.lua
@@ -1,5 +1,4 @@
 require('nvim-tree').setup {
-  hijack_netrw = true,
   view = {
     adaptive_size= true,
     mappings = {
@@ -27,3 +26,20 @@ require('nvim-tree').setup {
     }
   }
 }
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
