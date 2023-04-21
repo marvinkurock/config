9b74861cfcb67867df915c208293f96f46facb67
MK <mk@me.de>
2023-04-21 14:53:51 +0000

    remove

--- vms/ubuntu/.config/polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-polybar --config=~/.config/polybar/config.ini example 2>&1 | tee /tmp/polybar.log & disown
-
-echo "Polybar launched"
--- vms/ubuntu/.config/scripts/parallels-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall -q prlcc
-killall -q prlcp
-prlcc & disown
-prlcp & disown
