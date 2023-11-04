7156682bbe5508edca90bbeed682b9184635f9c4
Marvin <marvin.kurock@tech-excellence.de>
2023-11-04 13:36:54 +0100

    misc plugin

---
+++ .config/nvim/lua/marvin/plugins/misc.lua
@@ -0,0 +1,69 @@
+return {
+  "nvim-lua/popup.nvim",   -- An implementation of the Popup API from vim in Neovim,
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins,
+
+  -- themes
+  'folke/tokyonight.nvim',
+  'catppuccin/nvim',
+
+  -- extensions
+  "preservim/vimux",
+  "moll/vim-bbye",
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {}
+    end
+  },
+  "voldikss/vim-floaterm",
+  'akinsho/toggleterm.nvim',
+
+  -- code addons
+  "windwp/nvim-autopairs",
+  "windwp/nvim-ts-autotag",
+  "numToStr/Comment.nvim",
+  { 'sindrets/diffview.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+  {
+    'akinsho/git-conflict.nvim',
+    version = "*",
+    config = function()
+      require('git-conflict').setup()
+    end
+  },
+  "norcalli/nvim-colorizer.lua",
+  'ThePrimeagen/harpoon',
+  'folke/todo-comments.nvim',
+  'mbbill/undotree',
+
+  -- lsp
+  'hashivim/vim-terraform',
+  "towolf/vim-helm",
+  'github/copilot.vim',
+  'tpope/vim-fugitive',
+  'junegunn/gv.vim',
+  'akinsho/flutter-tools.nvim',
+  "jose-elias-alvarez/nvim-lsp-ts-utils",
+  "onsails/lspkind.nvim",
+  "ray-x/lsp_signature.nvim",
+  {
+    "glepnir/lspsaga.nvim",
+    config = function()
+      require('lspsaga').setup({
+        lightbulb = {
+          enable = false,
+          enable_in_insert = false,
+          sign = false,
+          sign_priority = 40,
+          virtual_text = false,
+        },
+      })
+    end
+  },
+  --
+  -- DAP protocol
+  "mfussenegger/nvim-dap",
+  "leoluz/nvim-dap-go",
+  "rcarriga/nvim-dap-ui",
+  "theHamsta/nvim-dap-virtual-text",
+  "nvim-telescope/telescope-dap.nvim"
+}
