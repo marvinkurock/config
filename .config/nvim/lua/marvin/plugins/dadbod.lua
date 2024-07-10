0dfb2e4c81f7f6109d0c1d8cf85cae9b770239d3
Marvin <marvin@kurock.io>
2024-07-10 16:27:08 +0200

    dadbod

---
+++ .config/nvim/lua/marvin/plugins/dadbod.lua
@@ -0,0 +1,24 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = "~/.dadbot"
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "dbout",
+      callback = function()
+        vim.wo.foldenable = false
+      end,
+    })
+  end,
+}
