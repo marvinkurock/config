b642d765ce7e7b154d680e6613aac05a8df5cb1e
Marvin <marvin@kurock.io>
2024-12-06 09:45:31 +0100

    update

--- .config/nvim/lua/marvin/plugins/lsp.lua
+++ .config/nvim/lua/marvin/plugins/lsp.lua
@@ -25,7 +25,7 @@ return {
     local ensure_servers = {
       "bashls",
       "html",
-      "tsserver",
+      "ts_ls",
       "yamlls",
     }
 
@@ -173,11 +173,11 @@ return {
     -- ADDITIONAL FILETYPES
     vim.filetype.add({ extension = { templ = "templ" } })
 
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    cmp.event:on(
-      'confirm_done',
-      cmp_autopairs.on_confirm_done()
-    )
+    -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    -- cmp.event:on(
+    --   'confirm_done',
+    --   cmp_autopairs.on_confirm_done()
+    -- )
 
     -- SIGNS
     local signs = {
